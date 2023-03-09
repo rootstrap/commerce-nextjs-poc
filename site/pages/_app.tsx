e058e19aed33e1bf0b32d5399625e04a39e32dbb
Fernando Rosenblit <fernandorosenblit@gmail.com>
2023-03-09 10:33:13 -0300

    Initial graphql codegen setup

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -2,19 +2,22 @@ import '@assets/main.css'
 import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
 
-import { FC, ReactNode, useEffect } from 'react'
+import { FC, ReactNode, useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
 
-const queryClient = new QueryClient()
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop
+  const [queryClient] = useState(() => new QueryClient())
 
   useEffect(() => {
     document.body.classList?.remove('loading')
@@ -23,13 +26,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Head />
-        <ManagedUIContext>
-          <Layout pageProps={pageProps}>
-            <Component {...pageProps} />
-          </Layout>
-        </ManagedUIContext>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Head />
+          <ManagedUIContext>
+            <Layout pageProps={pageProps}>
+              <Component {...pageProps} />
+            </Layout>
+          </ManagedUIContext>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Hydrate>
       </QueryClientProvider>
     </>
   )
--- site/pages/test.tsx
+++ site/pages/test.tsx
@@ -1,11 +1,33 @@
-import useProducts from 'api/hooks/products/useProducts'
+import { QueryClient, dehydrate } from '@tanstack/react-query'
+import { client } from '@shopify/client'
+import { useGetProductsQuery } from '@shopify/generated/graphql'
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient()
+
+  await queryClient.fetchQuery(
+    useGetProductsQuery.getKey(),
+    useGetProductsQuery.fetcher(client)
+  )
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  }
+}
 
 export default function Test() {
-  const { data, isLoading } = useProducts()
+  const { data, isLoading } = useGetProductsQuery(client)
 
   if (isLoading) {
     return <h2>Loading...</h2>
   }
 
-  return <div className="bg-white"></div>
+  return (
+    <div className="bg-white">
+      {data?.products.edges.map((product) => (
+        <h6 key={product.node.id}>{product.node.title}</h6>
+      ))}
+    </div>
+  )
 }
